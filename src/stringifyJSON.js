5eb23f6ac2b64bf2bb736ea91174c0471551015f
dierat <diedra.rater@gmail.com>
2015-06-10 11:40:03 -0700

    adds ugly but working JSON.stringify function code

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,14 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-	if (typeof obj === 'number') {
-		return obj.toString();
-	} else if (obj === null) {
-		return 'null';
-	} else if (typeof obj === 'string'){
-		return '"' + obj + '"';
-	} else if (Array.isArray(obj)){
-		return '[' + obj.toString() + ']';
-	}
-	return obj.toString();
+  if (typeof obj === 'function'){
+  	return '';
+  } else if (typeof obj === 'undefined'){
+    return '';
+  } else if (typeof obj === 'number') {
+    return obj.toString();
+  } else if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)){
+    if (obj.length === 0) {
+      return '[' + ']';
+    } else {
+      var str = '[';
+      for (var i=0; i<obj.length; i++){
+        str += stringifyJSON(obj[i]);
+        if (i !== obj.length-1){
+          str += ',';
+        }
+      }
+      return str += ']';
+    }	
+  } else if (typeof obj === 'object'){
+  	var str = '{';
+  	for (var key in obj){
+  		if (typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined'){
+  		  str += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+  		}
+  	}
+  	if (str.length > 1){
+      str = str.slice(0, str.length-1);
+  	}
+  	return str += '}';
+  }
+  return obj.toString();
 };
